d2410b3f598d4a6b1e68a0680ce6c950328d8236
alyssa chioldin <achioldi@ucm.es>
2025-10-11 15:05:29 +0200

    Update Controller.java

--- src/tp1/control/Controller.java
+++ src/tp1/control/Controller.java
@@ -106,7 +106,6 @@ public class Controller {
 			view.showEndMessage();
 		}
 
-		
 	}
 
 
@@ -116,73 +115,4 @@ public class Controller {
 
 
 
-/*public class Controller {
 
-	private Game game;
-	private GameView view;
-
-	public Controller(Game game, GameView view) {
-		this.game = game;
-		this.view = view;
-	}
-
-
-	/**
-	 * Runs the game logic, coordinate Model(game) and View(view)
-	 * 
-	 */
-	/*public void run() {
-		
-		view.showWelcome();
-		String[] inputs;
-
-		// while the game has not finished (either by winning or losing) and the command is != exit or != e (shortcut)
-		while(!game.playerLoses() && !game.playerWins()) {
-			
-			view.showGame();
-			
-			inputs = view.getPrompt();
-			
-			while (inputs.length > 1) {
-				view.showError(Messages.COMMAND_INCORRECT_PARAMETER_NUMBER);
-				inputs = view.getPrompt();
-			}
-
-			for(int i = 0; i < inputs.length; i++)
-				inputs[i] = inputs[i].toLowerCase(); // every letter is taken to lowercase so even words as "HeLp" are processed
-
-				switch(inputs[0]) {
-				case Messages.COMMAND_EXIT_NAME:
-				case Messages.COMMAND_EXIT_SHORTCUT:
-					break;		// if the first character is exit then go out of the while immediately?¿
-				
-				case Messages.COMMAND_HELP_NAME:
-				case Messages.COMMAND_HELP_SHORTCUT:
-					System.out.println(Messages.HELP);		
-					System.out.println();	
-					break;
-				
-				case Messages.COMMAND_RESET_NAME:
-				case Messages.COMMAND_RESET_SHORTCUT:
-					game.resetGame();
-					view.showGame();
-					break;
-					
-				case Messages.COMMAND_UPDATE_NAME:
-				case Messages.COMMAND_UPDATE_HELP:
-				case Messages.EMPTY:
-					game.update();
-					view.showGame();
-					break;
-					
-				default:
-					view.showError(Messages.UNKNOWN_COMMAND.formatted(inputs[0]));
-					break;
-				} 
-				
-				inputs = view.getPrompt();		// check if I can put it here without having an infinite loop
-		}
-		
-		view.showEndMessage();
-	}	
-}*/

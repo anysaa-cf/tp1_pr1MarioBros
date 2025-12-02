e2b88c9d6271adcbc4f1d75b09dd9c42c3e96e46
alyssa chioldin <achioldi@ucm.es>
2025-12-02 19:46:08 +0100

    lolazo

--- src/tp1/logic/GameConfiguration.java
+++ src/tp1/logic/GameConfiguration.java
@@ -1,8 +1,14 @@
 package tp1.logic;
 
+import java.util.List;
+
+import tp1.logic.gameobjects.Mario;
+
 public interface GameConfiguration {
 	public int remainingTime();
 	public int points();
 	public int numLives();
+	public Mario getMario();
+	public List<GameObject> getNPCObjects();
 	
 }

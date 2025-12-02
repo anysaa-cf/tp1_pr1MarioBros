991c3b20d68dd66aabab77a5ac99b40470b19b0e
alyssa chioldin <achioldi@ucm.es>
2025-12-02 19:41:57 +0100

    Update FileGameConfiguration.java

--- src/tp1/logic/FileGameConfiguration.java
+++ src/tp1/logic/FileGameConfiguration.java
@@ -1,7 +1,11 @@
 package tp1.logic;
 
-public class FileGameConfiguration implements GameConfiguration {
+import tp1.exceptions.GameLoadException;
 
+public class FileGameConfiguration implements GameConfiguration {
+	public FileGameConfiguration(String fileName, GameWorld game) throws GameLoadException {
+	}
+	
 	@Override
 	public int remainingTime() {
 		// TODO Auto-generated method stub

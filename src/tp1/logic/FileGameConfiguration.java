a2ba4b5379deb014015120d25530fbe7735e5154
alyssa chioldin <achioldi@ucm.es>
2025-12-02 20:12:51 +0100

    Update FileGameConfiguration.java

--- src/tp1/logic/FileGameConfiguration.java
+++ src/tp1/logic/FileGameConfiguration.java
@@ -6,31 +6,34 @@ import tp1.exceptions.GameLoadException;
 import tp1.logic.gameobjects.Mario;
 
 public class FileGameConfiguration implements GameConfiguration {
+	private int remainingTime;
+	private int points;
+	private int lives;
+	
+	private Mario mario;
+    private List<GameObject> npcObjects;
+	
 	public FileGameConfiguration(String fileName, GameWorld game) throws GameLoadException {
 	}
 	
 	@Override
 	public int remainingTime() {
-		// TODO Auto-generated method stub
-		return 0;
+		return remainingTime;
 	}
 
 	@Override
 	public int points() {
-		// TODO Auto-generated method stub
-		return 0;
+		return points;
 	}
 
 	@Override
 	public int numLives() {
-		// TODO Auto-generated method stub
-		return 0;
+		return lives;
 	}
 
 	@Override
 	public Mario getMario() {
-		// TODO Auto-generated method stub
-		return null;
+		return mario;
 	}
 
 	@Override
